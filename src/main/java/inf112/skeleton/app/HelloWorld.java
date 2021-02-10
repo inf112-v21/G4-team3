18adc75f6580936929f963de088e9b1a7e1bf19f
Eivind Skarstein <eivind.skarstein@student.uib.no>
2021-02-10 23:23:48 +0100

    Updated Testmap and HelloWorld
    Fikset et layer problem på Testmap.tmx
    Oppdaterte HelloWorld til å være lettere å lese

--- src/main/java/inf112/skeleton/app/HelloWorld.java
+++ src/main/java/inf112/skeleton/app/HelloWorld.java
@@ -23,28 +23,36 @@ public class HelloWorld extends InputAdapter implements ApplicationListener {
     private SpriteBatch batch;
     private BitmapFont font;
 
-    // Tutorial variables
+    // Layer variables
     private TiledMap map;
-    private TiledMapTileLayer boardLayer;
-    private TiledMapTileLayer playerLayer;
-    private TiledMapTileLayer holeLayer;
-    private TiledMapTileLayer flagLayer;
+    public TiledMapTileLayer boardLayer;
+    public TiledMapTileLayer playerLayer;
+    public TiledMapTileLayer holeLayer;
+    public TiledMapTileLayer flagLayer;
 
     private OrthographicCamera camera;
     private OrthogonalTiledMapRenderer renderer;
-    private int MAP_SIZE_X = 5;
-    private int MAP_SIZE_Y = 5;
-    private float widthPixels = 300;
-    private float heightPixels = 300;
+    private final int MAP_SIZE_X = 5;
+    private final int MAP_SIZE_Y = 5;
+    private final int widthPixels = 300;
+    private final int heightPixels = 300;
 
     // Displaying player
-    private Cell playerCell;
-    private Cell playerWonCell;
-    private Cell playerDiedCell;
-    private Vector2 playerPos = new Vector2(1,1);
+    public Cell playerCell;
+    public Cell playerWonCell;
+    public Cell playerDiedCell;
+    public Vector2 playerPos = new Vector2(0,0);
+
+    // End condition
+    public boolean loseCondition = false;
+    public boolean winCondition = false;
+
+    // Pause
+    public boolean pause = false;
 
     @Override
     public void create() {
+        // Setup text
         batch = new SpriteBatch();
         font = new BitmapFont();
         font.setColor(Color.RED);
@@ -59,18 +67,20 @@ public class HelloWorld extends InputAdapter implements ApplicationListener {
         // Setup camera
         camera = new OrthographicCamera();
         camera.setToOrtho(false, MAP_SIZE_X, MAP_SIZE_Y);
-        camera.position.x= 2.5F;
+        camera.position.x= MAP_SIZE_X/2f;
+        camera.position.x= MAP_SIZE_Y/2f;
         camera.update();
 
         // Setup render
-        renderer = new OrthogonalTiledMapRenderer(map, 1/300F);
+        renderer = new OrthogonalTiledMapRenderer(map, 1F/widthPixels);
         renderer.setView(camera);
 
-        // Create textures for different player states
+        // Split player.png into 3 textures
         Texture texture = new Texture("assets/player.png");
         TextureRegion texturePlayer = new TextureRegion(texture);
-        TextureRegion[][] tP = texturePlayer.split(texture,300,300);
+        TextureRegion[][] tP = texturePlayer.split(texture,widthPixels,heightPixels);
 
+        // Get the textures for the different player states
         StaticTiledMapTile playerCellTile = new StaticTiledMapTile(tP[0][0]);
         StaticTiledMapTile playerDiedCellTile = new StaticTiledMapTile(tP[0][1]);
         StaticTiledMapTile playerWonTile = new StaticTiledMapTile(tP[0][2]);
@@ -80,10 +90,7 @@ public class HelloWorld extends InputAdapter implements ApplicationListener {
         playerDiedCell = new Cell().setTile(playerDiedCellTile);
         playerWonCell = new Cell().setTile(playerWonTile);
 
-        // Start position
-        playerPos.set(1,1);
-
-        // Moving player
+        // Get key input(?)
         Gdx.input.setInputProcessor(this);
     }
 
@@ -91,24 +98,29 @@ public class HelloWorld extends InputAdapter implements ApplicationListener {
     @Override
     public boolean keyUp(int keyCode){
 
-        // Placeholder ----
-        playerLayer.setCell((int)playerPos.x, (int)playerPos.y, null);
-        if (keyCode == 19){
-            playerPos.y = playerPos.y + 1;
+        if(!pause) {
+            // -------- Placeholder movement --------
+            // Using arrow keys
+            playerLayer.setCell((int) playerPos.x, (int) playerPos.y, null); // Remove player sprite before moving
+            // Move up
+            if (keyCode == 19) {
+                playerPos.y = playerPos.y + 1;
+            }
+            // Move down
+            else if (keyCode == 20) {
+                playerPos.y = playerPos.y - 1;
+            }
+            // Move left
+            else if (keyCode == 22) {
+                playerPos.x = playerPos.x + 1;
+            }
+            // Move right
+            else if (keyCode == 21) {
+                playerPos.x = playerPos.x - 1;
+            }
         }
-        else if (keyCode == 20){
-            playerPos.y = playerPos.y - 1;
-        }
-        else if (keyCode == 22){
-            playerPos.x = playerPos.x + 1;
-        }
-        else if (keyCode == 21){
-            playerPos.x = playerPos.x - 1;
-        }
-        // ------
-
+        // ------------------------------------------
         return true;
-
     }
 
     @Override
@@ -119,27 +131,59 @@ public class HelloWorld extends InputAdapter implements ApplicationListener {
 
     @Override
     public void render() {
-
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
 
-
         renderer.render();
+        updatePlayer();
+    }
 
-        // Check player cell layer position
-        playerLayer.setCell((int)playerPos.x,(int)playerPos.y,playerCell);
-        Cell bl = boardLayer.getCell((int)playerPos.x,(int)playerPos.y);
-        Cell hl = holeLayer.getCell((int)playerPos.x,(int)playerPos.y);
-        Cell fl = flagLayer.getCell((int)playerPos.x,(int)playerPos.y);
+    // Check if player is positioned on the map
+    public boolean validPlayerMapPos(Vector2 pos){
+        boolean checkX = playerPos.x<MAP_SIZE_X && playerPos.x>=0;
+        boolean checkY = playerPos.y<MAP_SIZE_Y && playerPos.y>=0;
+        return checkX && checkY;
+    }
 
-        if (bl != null){
-            if (hl != null){
-                playerLayer.setCell((int)playerPos.x,(int)playerPos.y,playerDiedCell);
-            }
-            else if (fl != null){
-                playerLayer.setCell((int)playerPos.x,(int)playerPos.y,playerWonCell);
-            }
+    // Update the player based on the current map layer
+    public void updatePlayer(){
+        // Set the player on the layer
+        playerLayer.setCell((int) playerPos.x, (int) playerPos.y, playerCell);
+
+        // Checks if the player is on one of the layer types. Cell is null if player is not on the layer type
+        Cell boardCell = boardLayer.getCell((int) playerPos.x, (int) playerPos.y);
+        Cell holeCell = holeLayer.getCell((int) playerPos.x, (int) playerPos.y);
+        Cell flagCell = flagLayer.getCell((int) playerPos.x, (int) playerPos.y);
+
+        // Check if player is in a hole or is outside the map. If true ends the game.
+        if (holeCell != null || !validPlayerMapPos(playerPos)) {
+            playerLayer.setCell((int) playerPos.x, (int) playerPos.y, playerDiedCell);
+            loseCondition = true;
+            endGame();
+
+            // End lose screen
+            batch.begin();
+            font.getData().setScale(4, 4);
+            font.draw(batch, "You lost", 140, 200);
+            batch.end();
+
+
+        // Check if player has reached a flag. If true ends the game
+        } else if (flagCell != null) {
+            playerLayer.setCell((int) playerPos.x, (int) playerPos.y, playerWonCell);
+            winCondition = true;
+            endGame();
+
+            // End win screen
+            batch.begin();
+            font.getData().setScale(4, 4);
+            font.draw(batch, "You won", 140, 200);
+            batch.end();
         }
+    }
+
+    public void endGame(){
+        pause = true;
     }
 
     @Override
@@ -148,13 +192,11 @@ public class HelloWorld extends InputAdapter implements ApplicationListener {
 
     @Override
     public void pause() {
+        pause = true;
     }
 
     @Override
     public void resume() {
+        pause = false;
     }
-
-
-
-
 }
